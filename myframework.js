6c953c1084d7b969174d8000a96dd5e19cc9fcf8
rdjones98 <23530094+rdjones98@users.noreply.github.com>
2020-01-09 06:42:53 -0600

    a

--- myframework.js
+++ myframework.js
@@ -210,7 +210,41 @@ function rightArrowClicked()
 	urlClicked(url);
 }
 
-// Load KeyListener.  This is outside of any funciton and should load when page loads
+/* Begin Cookie Management */
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+function checkCookie() {
+  var username = getCookie("username");
+  if (username != "") {
+   alert("Welcome again " + username);
+  } else {
+    username = prompt("Please enter your name:", "");
+    if (username != "" && username != null) {
+      setCookie("username", username, 365);
+    }
+  }
+}
+
+// Load KeyListener.  This is outside of any function and should load when page loads
 document.onkeydown = function(e) {
 switch (e.keyCode) {
 	case 37:
